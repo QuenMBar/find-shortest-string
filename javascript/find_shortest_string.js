6938b77bfac1658fa66e721219c0af1761456aee
QuenMBar <Qbeast360@gmail.com>
2021-03-16 14:05:22 -0700

    compleated

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,23 +1,64 @@
+const { performance } = require("perf_hooks");
+
+// 0.00037653399826958773
+// function findShortestString(arr) {
+//     let shortIndex = 0;
+//     arr.forEach((elem, ind, array) => {
+//         // console.log(`${elem}, ${ind}, ${array}, ${elem.length < arr[shortIndex].length}`);
+//         if (elem.length < arr[shortIndex].length) {
+//             shortIndex = ind;
+//         }
+//     });
+//     return arr[shortIndex];
+// }
+
+// 0.0003052189988270402
 function findShortestString(arr) {
-  // type your code here
+    return arr.reduce((a, b) => (a.length <= b.length ? a : b));
 }
 
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 'a'");
-  console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
+    // add your own tests in here
+    console.log("Expecting: 'a'");
+    console.log("=>", findShortestString(["aaa", "a", "bb", "ccc"]));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: 'hi'");
-  console.log("=>", findShortestString(['cat', 'hi', 'dog', 'an']));
+    console.log("Expecting: 'hi'");
+    console.log("=>", findShortestString(["cat", "hi", "dog", "an"]));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: 'lily'");
-  console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
+    console.log("Expecting: 'lily'");
+    console.log("=>", findShortestString(["flower", "juniper", "lily", "dadelion"]));
 
-  // BENCHMARK HERE
+    // BENCHMARK HERE
+    let arrayLen1 = 10;
+    let arrayLen2 = 100;
+
+    let averageTries = 100000;
+
+    let timesArr = [];
+
+    for (let i = 0; i < averageTries; i++) {
+        let array1 = [];
+        let array2 = [];
+        for (let j = 0; j < arrayLen1; j++) {
+            array1.push("a".repeat(Math.floor(Math.random() * (arrayLen1 * 3)) + 1));
+        }
+        for (let j = 0; j < arrayLen2; j++) {
+            array2.push("a".repeat(Math.floor(Math.random() * (arrayLen2 * 2)) + 1));
+        }
+        let t1 = performance.now();
+
+        let dummyVar1 = findShortestString(array1);
+        let dummyVar2 = findShortestString(array2);
+
+        let t2 = performance.now();
+        timesArr[i] = t2 - t1;
+    }
+    let allTimes = timesArr.reduce((a, b) => a + b);
+    console.log(allTimes / averageTries);
 }
 
 module.exports = findShortestString;
